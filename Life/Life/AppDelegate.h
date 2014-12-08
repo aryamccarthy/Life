ab13320d2a78b01b0875822b515fc11675658a6f
aryamccarthy <arya.mccarthy@gmail.com>
2014-12-08 15:06:53 -0600

    Initialize Xcode project

---
+++ Life/Life/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Life
+//
+//  Created by Arya McCarthy on 12/8/14.
+//  Copyright (c) 2014 Arya McCarthy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
