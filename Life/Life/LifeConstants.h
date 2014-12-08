7855f87f3c5494343f140fde0fe42a9214a1e019
aryamccarthy <arya.mccarthy@gmail.com>
2014-12-08 15:11:11 -0600

    Constants

---
+++ Life/Life/LifeConstants.h
@@ -0,0 +1,16 @@
+//
+//  LifeConstants.h
+//  Life
+//
+//  Created by Arya McCarthy on 12/8/14.
+//  Copyright (c) 2014 Arya McCarthy. All rights reserved.
+//
+
+#ifndef Life_LifeConstants_h
+#define Life_LifeConstants_h
+
+const size_t kMaxAge = 12;
+const size_t kNumRows = 60;
+const size_t kNumCols = 40;
+
+#endif
