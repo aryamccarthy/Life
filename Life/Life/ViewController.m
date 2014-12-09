1d5fa71aba12b871096ae85bb98d8e26aba61914
aryamccarthy <arya.mccarthy@gmail.com>
2014-12-08 22:51:26 -0600

    Timing options

--- Life/Life/ViewController.m
+++ Life/Life/ViewController.m
@@ -14,7 +14,8 @@
 @interface ViewController ()
 @property (nonatomic, strong) LifeModel *model;
 @property (weak, nonatomic) IBOutlet UIView *container;
-@property (strong, nonatomic) NSMutableArray *cells; // of UIView
+@property (strong, nonatomic) NSTimer *timer;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *speedLabel;
 @end
 
 @implementation ViewController
@@ -30,17 +31,36 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
   // Do any additional setup after loading the view, typically from a nib.
+  self.navigationController.navigationBar.translucent = YES;
   for (int i = 0; i < self.model.size.height; ++i) {
     for (int j = 0; j < self.model.size.width; ++j) {
       UIView *cell = [[UIView alloc] initWithFrame:CGRectMake(8 * j, 8 * i, 8, 8)];
-      cell.backgroundColor = [UIColor colorWithRed:13/255.0 green:112/255.0 blue:0/255.0 alpha:1.0];
-      //cell.opaque = NO;
+      cell.backgroundColor = self.view.window.tintColor;//[UIColor colorWithRed:13/255.0 green:112/255.0 blue:0/255.0 alpha:1.0];
       cell.alpha = [[self.model valueAtRow:i and:j] doubleValue]/kMaxAge;
       [self.container addSubview:cell];
     }
   }
-  NSTimer* timer = [NSTimer timerWithTimeInterval:1.0f target:self selector:@selector(update) userInfo:nil repeats:YES];
-  [[NSRunLoop mainRunLoop] addTimer:timer forMode:NSRunLoopCommonModes];
+  [self speedButtonPressed:self.speedLabel];
+}
+
+- (IBAction)speedButtonPressed:(UIBarButtonItem *)sender {
+  if ([sender.title isEqualToString:@"Slow"]) {
+    sender.title = @"Fast";
+    [self.timer invalidate];
+    self.timer = [NSTimer timerWithTimeInterval:0.25f target:self selector:@selector(update) userInfo:nil repeats:YES];
+    [[NSRunLoop mainRunLoop] addTimer:self.timer forMode:NSRunLoopCommonModes];
+  }
+  else if ([sender.title isEqualToString:@"Fast"]) {
+    sender.title = @"Paused";
+    [self.timer invalidate];
+    self.timer = nil;
+  }
+  else if ([sender.title isEqualToString:@"Paused"]) {
+    sender.title = @"Slow";
+    [self.timer invalidate];
+    self.timer = [NSTimer timerWithTimeInterval:1.0f target:self selector:@selector(update) userInfo:nil repeats:YES];
+    [[NSRunLoop mainRunLoop] addTimer:self.timer forMode:NSRunLoopCommonModes];
+  }
 }
 
 - (IBAction)resetPressed:(UIBarButtonItem *)sender {
