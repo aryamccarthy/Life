f5e00191f25115d730d1bc408f42cf1fdd9bf907
aryamccarthy <arya.mccarthy@gmail.com>
2014-12-09 01:25:37 -0600

    addSubview is slow, so we'll use fewer cells.

--- Life/Life/LifeConstants.h
+++ Life/Life/LifeConstants.h
@@ -13,7 +13,7 @@ static const size_t kMaxAge = 12;
 
 // This times kNumRows and kNumCols should
 // equal the dimensions of the view.
-static const size_t kCellDimension = 8;
+static const size_t kCellDimension = 16;
 
 static const size_t kNumRows = 504/kCellDimension;
 static const size_t kNumCols = 320/kCellDimension;
